e06bda503923d5916d72d32d601a1c8fc6bedca7
Fabiyi Johnson <70953952+johnsiner@users.noreply.github.com>
2022-05-17 14:35:34 +0100

    Video-03-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+    return (
+        <div>
+            <div></div>
+            <header>
+                <a href="/">amazona</a>
+            </header>
+            <main>list product</main>
+        </div>
+    );
+}
+
+export default App;
